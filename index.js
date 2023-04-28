72c165ed8fa1d7e95e9f0f1a2f5e9f2e681ee6d9
Fatma Hussein <fartunhussein346@gmail.com>
2023-04-28 11:37:23 +0300

    Add javascript file for the hamburger events

---
+++ index.js
@@ -0,0 +1,11 @@
+// Hamburger Menu js events
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navbar');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
